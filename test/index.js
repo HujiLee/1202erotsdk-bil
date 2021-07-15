5c3618a60c91abc9f9c5a00a562b88a3d9e1b633
Hujimiya Dev Yudu <hujimiya@yandex.com>
2021-07-15 18:54:26 +0800

    上传时要注意store可能变成ks3sh

--- test/index.js
+++ test/index.js
@@ -3,7 +3,8 @@ const toolbox = require("../tool/toolbox");
 const path = require("path");
 const fs = require("fs");
 const CombinedStream = require('combined-stream');
-const rs = require("randomstring")
+const rs = require("randomstring");
+const buffer = require("buffer");
 
 LIB.GetKdstoreByWpssid("V02STM1ligb43rsiOc7StUsChFaOj-I00aced2b5003d18ab1d").then(async x => {
   // debugger
@@ -19,14 +20,28 @@ LIB.GetKdstoreByWpssid("V02STM1ligb43rsiOc7StUsChFaOj-I00aced2b5003d18ab1d").the
   let f1 = fs.createReadStream(p1);
   let f2 = fs.createReadStream(p2);
   cStream.append(f1).append(f2);
-  // let otest3 = await kd.POST_WPSFILE_KSYUN(cStream, combinedSize, "multipart/related", Math.random() + ".mht", 0)
-  debugger
+  cStream.
+    // let otest3 = await kd.POST_WPSFILE_KSYUN(cStream, combinedSize, "multipart/related", Math.random() + ".mht", 0)
+    debugger
   // let o_test = await kd.APIV5_files_file_asNewFile(otest3.data.etag,otest3.data.key,Math.random() + ".mht",otest3.data.newfilename_sha1,combinedSize,0);
   let dbgFolder = "124514263372";
   let otestest = await kd.APIV5_listfiles(dbgFolder);
-  let onefile = otestest.data.files.filter(e => e.ftype == "file"&&e.fname.endsWith("epub"))[0]
+  let onefile = otestest.data.files.filter(e => e.ftype == "file" && e.fname.endsWith("txt"))[0]
   let oup = await kd.POST_WPSFILE_KSYUN(cStream, combinedSize, 'application/octet-stream', onefile.fname, onefile.parentid);
   let onewver = await kd.APIV5_files_file_PUT_as_new_version(onefile.id, oup.data.key, oup.data.etag,
     oup.data.newfilename_sha1, combinedSize, oup.data.store);
+  for (let i = 1; i <= 50; i++) {
+    let st = CombinedStream.create();
+    let buf = buffer.Buffer.from(rs.generate({ length: 8000 }));
+    st.append(buf);
+    let upup = await kd.POST_WPSFILE_KSYUN(st, buf.byteLength, "application/octet-stream", onefile.fname, onefile.parentid);
+
+    let newversion = await kd.APIV5_files_file_PUT_as_new_version(onefile.id, upup.data.key, upup.data.etag, upup.data.newfilename_sha1, buf.byteLength,
+      upup.data.store);
+    if (upup.data.store == "ks3sh") {
+      debugger
+    }
+    // debugger
+  }
   debugger
 })
