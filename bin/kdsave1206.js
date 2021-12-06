513761eb60343d8c6b336649628f5e29e51cb6e3
Hujimiya Dev Yudu <hujimiya@yandex.com>
2021-12-06 23:12:10 +0800

    cli切换到正式上传

--- bin/kdsave1206.js
+++ bin/kdsave1206.js
@@ -13,8 +13,12 @@ const LIB = require("../lib");
 
 
 getUploadPaths(cwd, sub).then(async avai_paths => {
-  let o_kd = await LIB.GetKdstoreByWpssid('V02S2W_2E4DqgA19ZhRJs2B7EAW4NtY00a1449b3004da54f06');
-  process.env['KdocsUserZhushi'] = "长沙王牌沙雕测试"
+  // let o_kd = await LIB.GetKdstoreByWpssid('V02S2W_2E4DqgA19ZhRJs2B7EAW4NtY00a1449b3004da54f06');
+  // process.env['KdocsUserZhushi'] = "长沙王牌沙雕测试"
+  // const PARENT_ID = 145458924596;
+  let o_kd = await LIB.GetKdstoreByWpssid('V02Slsu21U9o4HXsTgfiy_XTvNpxYvM00a07e2cb003d18ab1d');
+  process.env['KdocsUserZhushi'] = "会员号ID 1025026845绑定小米6336839973"
+  const PARENT_ID = 145473224641;
   if (!o_kd.ok) {
     console.log("kd get fail:" + o_kd.msg)
     return process.exit(0);
@@ -22,7 +26,7 @@ getUploadPaths(cwd, sub).then(async avai_paths => {
   let kd = o_kd.data.kdv2021;
   let tasks = avai_paths.filter(e => e.stats.isDirectory()).map(ap => async cb => {
     console.log("尝试上传文件夹", ap.full_path)
-    let upload = await kd.app.v2.uploadUploadifiedDir(ap.full_path, 145458924596);
+    let upload = await kd.app.v2.uploadUploadifiedDir(ap.full_path, PARENT_ID);
     if (upload.ok) {
       console.log("OKDONE", ap.full_path)
     } else {
